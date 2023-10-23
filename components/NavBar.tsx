7072bb690be32712e2fb0b9ddec111e774dc86b2
Francoparodi01 <francoparodi2001@gmail.com>
2023-10-23 18:12:03 -0300

    main layout

---
+++ components/NavBar.tsx
@@ -0,0 +1,25 @@
+import { UserButton } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { MobileSidebar } from "./mobile-sidebar";
+
+
+
+const font = Poppins({ weight: "600", subsets: ["latin"] });
+
+export const Navbar = async () => {
+  return ( 
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10">
+      <div className="flex items-center">
+        <MobileSidebar/>
+        <h1 className={cn("text-xl md:text-4xl font-bold text-primary", font.className)}>
+          companion.ai
+        </h1>
+      </div>
+      <div className="flex">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+}
---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,18 @@
+import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import SideBar from "./sidebar";
+
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 w-36">
+        <SideBar/>
+      </SheetContent>
+    </Sheet>
+  );
+};
